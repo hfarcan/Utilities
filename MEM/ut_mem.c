8efe864dd9874adaa2515912bbcf31500950ac02
Habib Furkan Arcan <hf.arcan@gmail.com>
2024-09-22 22:27:03 +0300

    Update ut_mem.c

--- MEM/ut_mem.c
+++ MEM/ut_mem.c
@@ -2,38 +2,38 @@
 
 void	MemMove(tU8* pDst, tU8* pSrc, tU16 Len)
 {
-			if(pDst > pSrc)
-			{
-				pDst += Len;
-				pSrc += Len;
-				while(Len--)
-					*(--pDst) = *(--pSrc);
-			}
-			else
-			{
-				while(Len--)
-					*pDst++ = *pSrc++;			
-			}
+	if(pDst > pSrc)
+	{
+		pDst += Len;
+		pSrc += Len;
+		while(Len--)
+			*(--pDst) = *(--pSrc);
+	}
+	else
+	{
+		while(Len--)
+			*pDst++ = *pSrc++;			
+	}
 }
 
 void	MemSet(tU8* pDst, tU8 Val, tU16 Len)
 {
-			while(Len--)
-				*pDst++ = Val;			
+	while(Len--)
+		*pDst++ = Val;			
 }
 
 tS16	MemCompare(tU8* pMemA, tU8* pMemB, tU16 Len)
 {
 tS16	ret;
 
-			ret = 0;
-			while(Len--)
-			{
-				ret += *pMemA++ - *pMemB++;
-				if(ret)
-					break;
-			}
-			return ret;
+	ret = 0;
+	while(Len--)
+	{
+		ret += *pMemA++ - *pMemB++;
+		if(ret)
+			break;
+	}
+	return ret;
 }
 
 tS16 MemPutStr(tS8* pDst, void* pSrc)
@@ -41,62 +41,62 @@ tS16 MemPutStr(tS8* pDst, void* pSrc)
 tU8* p;
 tS16 len;	
 
-		p = pSrc;
-		len = 0;
-		while(p && *p)
-		{
-			*pDst++ = *p++;	
-			len++;
-		}
-		*pDst = 0x00;
-		return len;
+	p = pSrc;
+	len = 0;
+	while(p && *p)
+	{
+		*pDst++ = *p++;	
+		len++;
+	}
+	*pDst = 0x00;
+	return len;
 }
 
 void MemPutChar(tS8* pDst, tU8 Char)
 {
-		if(pDst)
-		{
-			*pDst = Char;	
-		}
+	if(pDst)
+	{
+		*pDst = Char;	
+	}
 }
 
 void	MemPutU16(tU8* pDst, tU16 Val)
 {
-			*pDst++ = Val >> 8;
-			*pDst = Val;
+	*pDst++ = Val >> 8;
+	*pDst = Val;
 }
 
 void	MemPutU16I(tU8* pDst, tU16 Val)
 {
-			*pDst++ = Val;
-			*pDst = Val >> 8;
+	*pDst++ = Val;
+	*pDst = Val >> 8;
 }
 
 void	MemPutS16(tU8* pDst, tS16 Val)
 {
-			*pDst++ = Val >> 8;
-			*pDst = Val;
+	*pDst++ = Val >> 8;
+	*pDst = Val;
 }
 
 void	MemPutU32(tU8* pDst, tU32 Val)
 {
-			*pDst++ = Val >> 24;
-			*pDst++ = Val >> 16;
-			*pDst++ = Val >> 8;
-			*pDst = Val;
+	*pDst++ = Val >> 24;
+	*pDst++ = Val >> 16;
+	*pDst++ = Val >> 8;
+	*pDst = Val;
 }
 
 void	MemPutU32I(tU8* pDst, tU32 Val)
 {
-			*pDst++ = Val;
-			*pDst++ = Val >> 8;
-			*pDst++ = Val >> 16;
-			*pDst = Val >> 24;
+	*pDst++ = Val;
+	*pDst++ = Val >> 8;
+	*pDst++ = Val >> 16;
+	*pDst = Val >> 24;
 }
 
 void	MemPutFLOAT(tU8* pDst, float Val)
 {
-			MemPutU32(pDst, *(tU32*) &Val);
+	MemPutU32(pDst, *(tU32*) &Val);
 }
 
 tS16 MemPutFlags(tS8* pDst,void* pStr, tU32 Flags)
@@ -105,35 +105,35 @@ tU16 i;
 tU8 nibble;	
 tS16 len;	
 	
-		len = MemPutStr(pDst,pStr);
-		pDst+=len;
-		for(i = 0; i<8; i++)
-		{
-			nibble = (0x0F)&(Flags>>((7-i)*4));
-			if(nibble<0x0A)
-				nibble = nibble + '0';
-			else
-				nibble = nibble - 10 + 'A';			
-			MemPutChar(pDst++, nibble);	
-		}
-		*pDst = 0x00;
-		len +=8;
+	len = MemPutStr(pDst,pStr);
+	pDst+=len;
+	for(i = 0; i<8; i++)
+	{
+		nibble = (0x0F)&(Flags>>((7-i)*4));
+		if(nibble<0x0A)
+			nibble = nibble + '0';
+		else
+			nibble = nibble - 10 + 'A';			
+		MemPutChar(pDst++, nibble);	
+	}
+	*pDst = 0x00;
+	len +=8;
 		
-		return len;
+	return len;
 }
 
 tS16 MemGetDigitCnt(tS32 Val)
 {
 tS16 len;
 
-		len = 0;
-		while(Val)
-		{
-			len++;
-			Val /= 10;
-		}
+	len = 0;
+	while(Val)
+	{
+		len++;
+		Val /= 10;
+	}
 		
-		return len;
+	return len;
 }
 
 tS16 MemPutS32Digits(tS8* pDst, tS32 Val, tU16 MinDigits)
@@ -182,10 +182,10 @@ tU16	MemGetU16(tU8* pSrc)
 {
 tU16	ret;
 			
-			ret = 0;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc;
-			return ret;
+	ret = 0;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc;
+	return ret;
 }
 
 
@@ -193,30 +193,22 @@ tS16	MemGetS16(tU8* pSrc)
 {
 tS16	ret;
 			
-			ret = 0;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc;
-			return ret;
+	ret = 0;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc;
+	return ret;
 }
-
-
-
-
-
-
-
-
 
 tU32	MemGetU32(tU8* pSrc)
 {
 tU32	ret;
 			
-			ret = 0;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc;
-			return ret;
+	ret = 0;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc;
+	return ret;
 }
 
 
@@ -224,12 +216,12 @@ tS32	MemGetS32(tU8* pSrc)
 {
 tS32	ret;
 			
-			ret = 0;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc++;	ret <<= 8;
-			ret += *pSrc;
-			return ret;
+	ret = 0;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc++;	ret <<= 8;
+	ret += *pSrc;
+	return ret;
 }
 
 float	MemGetFLOAT(tU8* pSrc)
@@ -237,9 +229,9 @@ float	MemGetFLOAT(tU8* pSrc)
 float	ret;
 tU32	val;	
 				
-			val = MemGetU32(pSrc);
-			ret = *(float*)&val;
-			return ret;
+	val = MemGetU32(pSrc);
+	ret = *(float*)&val;
+	return ret;
 }
 
 
